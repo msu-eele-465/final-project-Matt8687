e9022c376678d2fdb6484f9dd7617a72c343ad0d
M. Aiden Phillips <mphillips8687@gmail.com>
2025-04-30 18:01:41 -0600

    Started implementation, issues with the LCD outputting a random 'I'

--- esp32-controller/src/main.cpp
+++ esp32-controller/src/main.cpp
@@ -1,23 +1,62 @@
 #include <Arduino.h>
 #include <Wire.h>
 
+void clear();
+void startTop();
+
+byte error, address;
+
 void setup() {
-  Wire.begin();
   Serial.begin(115200);
-  Serial.println("\nI2C Scanner");
+  Serial.println("Serial Started.");
+
+  Serial.println("Starting I2C Wire...");
+  Wire.begin();
+  Wire.setClock(800000);
+  clear();
+  Serial.println("I2C Wire Started.");
+
 }
  
 void loop() {
-  byte error, address;
-  byte data[] = {"THow is you Matt"};
+  error = 1;
+  byte data[] = "How is you Matt ";
   int nDevices;
   address = 0x40;
+
+  startTop();
+
   Serial.println("Sending LCD Data");
   Wire.beginTransmission(address);
-  Wire.write(data, 16);
+  Wire.write(data, sizeof(data)-1);
+  Serial.println();
+  Serial.println(sizeof(data)-1);
   error = Wire.endTransmission();
   if (error == 0) {
     Serial.println("Unable to write to 0x40");
   }
-  sleep(1);
+  sleep(3);
+}
+
+void clear(){
+  address = 0x40;
+  Wire.beginTransmission(address);
+  for(int i = 0; i<16; i++){
+    Wire.write('z');
+  }
+  Wire.write('H');
+  Wire.write('G');
+  error = Wire.endTransmission();
+}
+
+void startTop(){
+  address = 0x40;
+  Wire.beginTransmission(address);
+  for(int i = 0; i<16; i++){
+    Wire.write('z');
+  }
+  Wire.write('H');
+  Wire.write('G');
+  Wire.write('T');
+  error = Wire.endTransmission();
 }
