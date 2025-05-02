de39ff0302c11cc7d69f787060548f6b1e514acc
M. Aiden Phillips <mphillips8687@gmail.com>
2025-05-02 15:34:18 -0600

    Initializing with testing code for scrolling text

---
+++ esp32-controller/src/led_array_control.cpp
@@ -0,0 +1,62 @@
+#include "led_array_control.h"
+
+#define DATA_PIN 18
+#define arr_len( x )  ( sizeof( x ) / sizeof( *x ) )
+
+// Matrix setup params
+Adafruit_NeoMatrix matrix = Adafruit_NeoMatrix(32, 16, DATA_PIN,
+    NEO_MATRIX_TOP    + NEO_MATRIX_LEFT +
+    NEO_MATRIX_COLUMNS + NEO_MATRIX_PROGRESSIVE + NEO_MATRIX_ZIGZAG,
+    NEO_GRB            + NEO_KHZ800);
+
+// The important stuff, your message and colors
+
+char* Text[]= {"Greetings Joe, how goes your project?       "};
+const uint16_t colors[] = {
+  matrix.Color(255, 0, 255), matrix.Color(100, 255, 0), matrix.Color(255, 255, 0),matrix.Color(0, 0, 255), matrix.Color(255, 0, 255), matrix.Color(0, 255, 255), matrix.Color(255, 255, 255)};
+int brightness = 40;
+
+int numMode = arr_len(Text)-1;
+int numColor = arr_len(colors)-1;
+int pixelPerChar = 4;
+int maxDisplacement;
+int mode = 0;
+
+void LAC_init() {
+    matrix.begin();
+    matrix.setTextWrap(false);
+    matrix.setBrightness(brightness);
+    matrix.setTextColor(colors[0]);
+}
+
+int y = matrix.height();
+int x = matrix.width();
+int pass = 0;
+int line_pass = 0;
+
+void LAC_update() {
+    // put your main code here, to run repeatedly:
+
+    // if mode greater than numMode reset
+    if (mode > numMode) { mode = 0; }
+
+    matrix.fillScreen(0);
+    matrix.setCursor(x, 0);
+    LAC_scroll(Text[mode],15);
+
+}
+
+// this does the magic of scrolling
+void LAC_scroll(char* message,int delays) {
+    maxDisplacement = strlen(message) * pixelPerChar + matrix.width();
+    if(++line_pass > matrix.width()) line_pass = 0;
+    matrix.print(String(message));
+    if(--x < -maxDisplacement) {
+        x = matrix.width();
+        if(++pass >= numColor) { pass = 0;  };
+        matrix.setTextColor(colors[pass]);
+        mode++;
+    }
+    matrix.show();
+    delay(delays);
+}
